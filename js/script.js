2811a9c1e07479cb595124eb7fc9abc5ab9dddc3
Cristal Muhammad <cristalmuhammad@gmail.com>
2022-03-23 17:37:01 -0700

    commit for assignment 5

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// could not get the text to display in console for lines 2-26
+// let id = document.getElementById('id') 
+// id.addEventListener('click', (e) => {
+//    console.log(`ID: ${e.target.id} `);
+// })
+
+// let empName = document.getElementById('empName') 
+// empName.addEventListener('click', (e) => {
+//    console.log(`Name: ${e.target.empName} `);
+// })
+
+// let ext= document.getElementById('ext') 
+// ext.addEventListener('click', (e) => {
+//    console.log(`Extension: ${e.target.ext} `);
+// })
+
+// let email = document.getElementById('email') 
+// email.addEventListener('click', (e) => {
+//    console.log(`Email: ${e.target.email} `);
+// })
+ 
+// let department = document.getElementById('department') 
+// department.addEventListener('click', (e) => {
+//     preventDefault()
+//    console.log(`Department: ${e.target.department} `);
+// })
+
+// let sumbit = document.getElementById('sumbit')
+// sumbit.addEventListener('click', (e) => {
+//     console.log(e.target.submit)
+// } )
+
+
+// I suspect I was on the right track here but could not put the pieces together
+// let empName
+// let ext
+// let email
+// let department
+// let empForm
+
+// id          =$('id')
+// empName     =$('empName')
+// ext         =$('ext')
+// email       =$('email')
+// department  =$('deparment')
+// empForm     =$('empForm')
+
+// const $ = (id) => document.getElementById(id)
+
+// id.addEventListener('click', (e) => {
+//     console.log(`ID: ${e.target.id}`)
+// })
+
+// const $ = (id) => document.getElementById(id)
+
+// function submitAddEmployee (id) 
+// let id  = document.getElementById('id')
+//     id.addEventListener('click', (e) => {
+//         console.log(`ID: ${e.target.id} `);
+    
+// })
